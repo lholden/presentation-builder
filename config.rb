2da71e1d55f1acc4ff7b137df685ce77360e6459
Lori Holden <lholden@ypcmc03249.yellowpages.local>
2008-09-16 13:00:19 -0700

    Moved things around a touch.

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ module Presentation
     :layout_file => File.join('src', 'index.html.eruby'),
     :slide_dir => File.join('src', 'slides'),
     :example_dir => 'examples',
-    :destination_file => 'index.html',
+    :destination_file => File.join('public', 'index.html'),
     :markup => 'markdown', # configs filename. my_file.markdown.eruby for example
     :erubis_extension => 'eruby',
     :highlight_theme => 'sunburst'
